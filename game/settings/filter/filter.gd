e18771c46a7c842b71742e296475b6876412ae81
Gonzaa <gonzaahirsch@gmail.com>
2026-03-09 15:24:09 -0300

    Modify filter game
    Adding visibility range to the player. Add more fog. Remove particles

--- game/settings/filter/filter.gd
+++ game/settings/filter/filter.gd
@@ -1,12 +1,41 @@
 extends CanvasLayer
 
 var t := 0.0
+var shader_material: ShaderMaterial
 
 func _ready() -> void:
-	pass
+	if has_node("FilterParticles"):
+		shader_material = $FilterParticles.material
 
-# execute filter
 func _process(delta):
 	t += delta
-	if has_node("FilterParticles"):
-		$FilterParticles.material.set_shader_parameter("time", t)
+	
+	if not shader_material:
+		return
+	
+	shader_material.set_shader_parameter("time", t)
+	
+	_update_player_position()
+
+func _update_player_position():
+	var player = get_tree().get_first_node_in_group("player")
+	if not player:
+		return
+	
+	var viewport = get_viewport()
+	var viewport_rect = viewport.get_visible_rect()
+	var camera = viewport.get_camera_2d()
+	
+	if not camera:
+		print("No hay cámara")
+		return
+	
+	var canvas_transform = camera.get_canvas_transform()
+	var player_screen_pos = canvas_transform * player.global_position
+	
+	var normalized_pos = Vector2(
+		player_screen_pos.x / viewport_rect.size.x,
+		player_screen_pos.y / viewport_rect.size.y
+	)
+	
+	shader_material.set_shader_parameter("player_position", normalized_pos)
