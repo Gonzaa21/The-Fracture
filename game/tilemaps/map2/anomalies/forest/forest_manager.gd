138f7b14a3701ad5a6d2caed51f8fa9a92d2f21a
Gonzaa <gonzaahirsch@gmail.com>
2026-08-08 12:39:52 -0300

    Track portal entry side and update portal behavior
    Track which portal the player used. validate_choice now determine if the player went back, flips the current entry side, and selects the proper target marker (LevelEntrance/LevelExit).

--- game/tilemaps/map2/anomalies/forest/forest_manager.gd
+++ game/tilemaps/map2/anomalies/forest/forest_manager.gd
@@ -12,6 +12,9 @@ var has_anomaly: bool = false
 var current_anomaly: Anomaly = null
 var anomaly_pool: Array[Anomaly] = []
 
+enum EntrySide { LEFT, RIGHT }
+var current_entry_side: EntrySide = EntrySide.LEFT
+
 func _ready() -> void:
 	anomaly_pool.append(preload("res://game/tilemaps/map2/anomalies/trees/scale/trees_global_giant.tres"))
 	anomaly_pool.append(preload("res://game/tilemaps/map2/anomalies/trees/scale/trees_global_tiny.tres"))
@@ -30,12 +33,11 @@ func _ready() -> void:
 func start_forest(scene: Node, anomaly_spawner: AnomalySpawner):
 	current_scene = scene
 	spawner = anomaly_spawner
-	
 	current_level = 0
 	levels_completed = 0
+	current_entry_side = EntrySide.LEFT
 	
 	GameManager.change_music("res://assets/sound/music/Triptych III  Desolation - Bosque.mp3")
-	
 	generate_level()
 
 func generate_level():
@@ -50,8 +52,12 @@ func generate_level():
 		print("anomalia: ", current_anomaly.resource_path)
 	else: print("nada")
 
-func validate_choice(back: bool):
+func validate_choice(portal_crossed: EntrySide):
+	var back = (portal_crossed == current_entry_side)
+	var target_marker = "LevelExit" if portal_crossed == EntrySide.LEFT else "LevelEntrance"
 	var correct = (back == has_anomaly)
+	print("current entry side: ", current_entry_side)
+	current_entry_side = EntrySide.RIGHT if portal_crossed == EntrySide.LEFT else EntrySide.LEFT
 	
 	if correct:
 		print("crack")
@@ -59,10 +65,10 @@ func validate_choice(back: bool):
 		if levels_completed >= TOTAL_LEVELS:
 			exit_forest()
 		else:
-			next_level()
+			next_level(target_marker)
 	else:
 		print("bobi")
-		reset_progress()
+		reset_progress(target_marker)
 
 func play_echo():
 	if unlock_echo_sound:
@@ -74,32 +80,33 @@ func play_echo():
 		echo_player.play()
 		echo_player.finished.connect(func(): echo_player.queue_free())
 
-func reset_player_position():
+func reset_player_position(target_marker_name: String):
 	var player = spawner.pool.get_player()
-	var entrance = current_scene.get_node("LevelEntrance")
-	if player and entrance:
-		player.global_position = entrance.global_position
+	var target = current_scene.get_node(target_marker_name)
+	if player and target:
+		player.global_position = target.global_position
 
-func next_level():
-	TransitionEffect.fade_in_place(func():
-		spawner.restore_state()
-		reset_player_position()
-		play_echo()
-		generate_level()
-	)
+func next_level(target_marker: String = "LevelEntrance"):
+	spawner.restore_state()
+	reset_player_position(target_marker)
+	play_echo()
+	generate_level()
+	var player = spawner.pool.get_player()
+	var camera: Camera2D = player.get_node("Camera2D")
+	camera.reset_smoothing()
 
-func reset_progress():
-	TransitionEffect.fade_in_place(func():
-		spawner.restore_state()
-		reset_player_position()
-		levels_completed = 0
-		current_level = 0
-		generate_level()
-	)
+func reset_progress(target_marker: String = "LevelEntrance"):
+	spawner.restore_state()
+	reset_player_position(target_marker)
+	levels_completed = 0
+	current_level = 0
+	generate_level()
+	var player = spawner.pool.get_player()
+	var camera: Camera2D = player.get_node("Camera2D")
+	camera.reset_smoothing()
 
 func exit_forest():
 	print("saliste pa")
 	GameManager.change_music("res://assets/sound/music/River's Bend - background_act3.mp3")
-	await get_tree().create_timer(2.0).timeout
 	TransitionEffect.fade_to_scene("res://game/tilemaps/map3/level_3.tscn")
 	pass
--- game/tilemaps/map2/anomalies/forest/portal.gd
+++ game/tilemaps/map2/anomalies/forest/portal.gd
@@ -8,8 +8,7 @@ func _on_body_entered(body: Node2D):
 	if body.is_in_group("player"):
 		set_deferred("monitoring", false)
 		
-		var back = (portal_type == PortalType.LEFT)
-		ForestManager.validate_choice(back)
+		ForestManager.validate_choice(portal_type as ForestManager.EntrySide)
 		
 		await get_tree().create_timer(5).timeout
 		monitoring = true
